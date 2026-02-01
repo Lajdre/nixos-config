b4ae2701df49dc2bc2835b8d798f8a7331a572ff
Lajdre <AndreAndreM@proton.me>
2026-02-01 16:55:00 +0100

    nvim: migrate to nvim-treesitter main branch setup

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,58 +1,11 @@
 return {
   'nvim-treesitter/nvim-treesitter',
+  lazy = false,
   dev = vim.g.nixed ~= nil,
-  event = { 'BufReadPost', 'BufNewFile' },
   config = function()
-    local ensure_installed_tree = {}
-    if vim.g.nixed == nil then
-      vim.list_extend(ensure_installed_tree, {
-        'python',
-        'rust',
-        'c',
-        'cpp',
-        'lua',
-        'typescript',
-        'tsx',
-        'markdown',
-        'markdown_inline',
-        'vim',
-        'vimdoc',
-        'regex',
-        'bash',
-        'yaml',
-        'sql',
-      })
-    end
-    require('nvim-treesitter.configs').setup({
-      auto_install = false,
-      ensure_installed = ensure_installed_tree,
-      ignore_install = {},
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-        custom_captures = {},
-        disable = {},
-      },
-      incremental_selection = {
-        enable = false,
-        disable = {},
-        keymaps = {
-          init_selection = 'gnn',
-          node_decremental = 'grm',
-          node_incremental = 'grn',
-          scope_incremental = 'grc',
-        },
-      },
-      indent = {
-        enable = true,
-        disable = { 'markdown', 'python' },
-      },
-      disable = function(lang, buf)
-        local max_filesize = 100 * 1024 -- 100 KB
-        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-        if ok and stats and stats.size > max_filesize then
-          return true
-        end
+    vim.api.nvim_create_autocmd('FileType', {
+      callback = function(args)
+        pcall(vim.treesitter.start, args.buf)
       end,
     })
   end,
