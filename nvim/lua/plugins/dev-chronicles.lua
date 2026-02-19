85d6fb0ea04bed6ce6be127e8880f08d12ec5bf6
Lajdre <AndreAndreM@proton.me>
2026-02-19 22:02:07 +0100

    nvim: update the dev-chronicles setup

--- nvim/lua/plugins/dev-chronicles.lua
+++ nvim/lua/plugins/dev-chronicles.lua
@@ -4,5 +4,31 @@ return {
   opts = {
     tracked_parent_dirs = { '~/projects/zzpackage/', '~/projects/' },
     tracked_dirs = { '~/nixos-config/' },
+    exclude_subdirs_relative = { 'zzsilent/' },
+    timeline = {
+      timeline_days = {
+        segment_abbr_labels = {
+          date_abbrs = { 'nd', 'pn', 'wt', 'śr', 'cz', 'pt', 'sb' },
+        },
+      },
+      timeline_months = {
+        segment_abbr_labels = {
+          date_abbrs = {
+            'styczń',
+            'luty',
+            'marzec',
+            'kwiecień',
+            'maj',
+            'czerwiec',
+            'lipiec',
+            'sierpień',
+            'wrzesień',
+            'paździer',
+            'listopad',
+            'grudzień',
+          },
+        },
+      },
+    },
   },
 }
