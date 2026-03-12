45f9a61f25df41801eae3b7e8b9239046f42eb32
Lajdre <AndreAndreM@proton.me>
2026-03-12 23:14:21 +0100

    nvim: add python keymaps

--- nvim/after/ftplugin/python.lua
+++ nvim/after/ftplugin/python.lua
@@ -8,3 +8,5 @@ vim.api.nvim_buf_set_keymap(0, 'i', 'qffp', 'print(f"}")<left><left><left>', { n
 vim.api.nvim_buf_set_keymap(0, 'i', ';', '<End>:<CR>', { noremap = true })
 vim.api.nvim_buf_set_keymap(0, 'i', 'q;', ';', { noremap = true })
 vim.api.nvim_buf_set_keymap(0, 'x', '<Leader>cp', ':g/print/s/^/#/\r', { noremap = true }) -- comment all lines starting with print
+vim.api.nvim_buf_set_keymap(0, 'i', 'qt', '# TODO: ', { noremap = true })
+vim.api.nvim_buf_set_keymap(0, 'i', 'qm', 'if __name__ == "__main__":<CR><Tab>main()<Esc>', { noremap = true })
