1de8220d9361191ca389d57ec79281062f336455
Lajdre <AndreAndreM@proton.me>
2026-03-13 14:55:05 +0100

    nixos: add home-manager user dirs config

---
+++ hmModules/userDirs/default.nix
@@ -0,0 +1,16 @@
+{ ... }:
+
+{
+  xdg.userDirs = {
+    enable = true;
+    createDirectories = false;
+    desktop = "$HOME";
+    documents = "$HOME";
+    download = "$HOME/downloads";
+    music = "$HOME";
+    pictures = "$HOME";
+    publicShare = "$HOME";
+    templates = "$HOME";
+    videos = "$HOME";
+  };
+}
