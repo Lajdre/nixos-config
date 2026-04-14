a7531b1bb6e90ee88e794621545aaa606dfb26ea
Lajdre <AndreAndreM@proton.me>
2026-04-14 15:18:11 +0200

    nixos: add dockerSocket for podman

--- modules/virtualisation.nix
+++ modules/virtualisation.nix
@@ -5,9 +5,8 @@
   virtualisation = {
     podman = {
       enable = true;
-      # Create a `docker` alias for podman, to use it as a drop-in replacement
       dockerCompat = true;
-      # Required for containers under podman-compose to be able to talk to each other.
+      dockerSocket.enable = true;
       defaultNetwork.settings.dns_enabled = true;
     };
   };
