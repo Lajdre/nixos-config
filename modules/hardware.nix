1d433f7f2038e6d9bd0e7715e41c87882c850bb1
Lajdre <AndreAndreM@proton.me>
2026-04-17 23:01:39 +0200

    nixos: disable powerOnBoot for the bluetooth service

--- modules/hardware.nix
+++ modules/hardware.nix
@@ -4,8 +4,10 @@
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
-  hardware.bluetooth.enable = true;
-  # hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot # https://nixos.wiki/wiki/Bluetooth
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = false;
+  };
 
   # https://discourse.nixos.org/t/how-to-create-files-in-the-etc-udev-rules-d-directory/11929
   # https://github.com/zsa/wally/wiki/Linux-install#2-create-a-udev-rule-file
