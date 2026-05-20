599b636f4926d2750adfdf5c7c739f0ae723ec24
Lajdre <AndreAndreM@proton.me>
2026-05-20 21:24:34 +0200

    nixos: add the monitor hyprland config for amir host

--- hmModules/hyprland/default.nix
+++ hmModules/hyprland/default.nix
@@ -4,11 +4,15 @@
   home.file.".config/hypr_per_host/conf/monitor.conf".text =
     if host == "master" then
       ''
-        monitor=,2560x1440@60, 0x0, 1
+        monitor = , 2560x1440@60, 0x0, 1
+      ''
+    else if host == "amir" then
+      ''
+        monitor = eDP-1, 2560x1600@180, 0x0, 1.25
       ''
     else
       ''
-        monitor=,1920x1080@60, 0x0, 1
+        monitor = , 1920x1080@60, 0x0, 1
       '';
 
   home.file.".config/hypr/" = {
