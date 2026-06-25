8540880011a3fd6d794498a2755581eea9111463
Lajdre <AndreAndreM@proton.me>
2026-06-25 11:41:54 +0200

    nixos: disable librewolf

--- hmModules/librewolf/default.nix
+++ hmModules/librewolf/default.nix
@@ -2,7 +2,7 @@
 
 {
   programs.librewolf = {
-    enable = true;
+    enable = false;
   };
   # https://github.com/contre95/.dotfiles/blob/d32832c4ba9dec37f4150742d9fc5f5537c0a19b/dotfiles/nixos/programs/librewolf.nix
 }
